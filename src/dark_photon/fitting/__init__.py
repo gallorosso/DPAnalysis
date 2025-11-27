6fa0a0b71d324121670073d16452959a0dd5da95
Andrea Gallo Rosso <gallorosso@users.noreply.github.com>
2025-11-27 13:26:33 +0100

    ... la volta buona.

--- src/dark_photon/fitting/__init__.py
+++ src/dark_photon/fitting/__init__.py
@@ -4,7 +4,7 @@ Fitting module for cavity analysis.
 
 from .lorentzian import lorentzian_plus_linear, fit_lorentzian, iq_to_magnitude
 from .cavity_fitter import cavity_fit
-from .range_optimizer import optimized_fit, optimized_fit_jpa
+from .range_optimizer import optimized_fit, optimized_fit_jpa, optimized_fit_jpa_strict
 
 __all__ = [
     'lorentzian_plus_linear',
@@ -13,5 +13,5 @@ __all__ = [
     'cavity_fit',
     'optimized_fit', 
     'optimized_fit_jpa',
-    'optimized_fit_jpa_strict'
+    'optimized_fit_jpa_strict',
 ]
